58f7cdbc0881ff9d8d59f4fc8ec772149dc02490
c-alys13 <ashleyycarilloo@gmail.com>
2026-05-01 02:19:10 +0800

    Add files via upload

---
+++ password entry (adjust width).py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Python Entry Widget")
+root.geometry("400x100")
+
+instruction_label = tk.Label(root, text="Enter your password:")
+instruction_label.pack(pady=10)
+
+entry = tk.Entry(root, width=50, show="*")
+entry.pack(pady=5)
+#show="":shows a certain character, likely ginagamit sya para itago iinput ng user for privacy purposes 
+
+root.mainloop()
